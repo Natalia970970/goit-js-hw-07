8bc7641ee95cb8fdf6e154c9ba048ccc6b87c474
Natalia970970 <103364315+Natalia970970@users.noreply.github.com>
2022-09-22 13:06:00 +0300

    02task done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+let images = galleryItems;
+
+const imgCard = ({preview, original, description}) => 
+    `<a class="gallery__item" href="${original}">
+        <img 
+            class="gallery__image" 
+            src="${preview}" 
+            alt="${description}"
+        />
+    </a>`;
+
+const gallery = document.querySelector('ul.gallery');
+
+const render = () => {
+    const list = images.map((item) => imgCard(item)).join('');
+    
+    gallery.insertAdjacentHTML('beforeend', list);
+};
+
+render();
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
